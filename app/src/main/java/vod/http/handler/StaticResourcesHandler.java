908f463568696573807035b6cdcbf47a985509ca
forDream <gam2046@gmail.com>
2015-12-28 16:23:34 +0800

    [fix]Static Res Handler

--- app/src/main/java/vod/http/handler/StaticResourcesHandler.java
+++ app/src/main/java/vod/http/handler/StaticResourcesHandler.java
@@ -24,31 +24,27 @@ public class StaticResourcesHandler implements RequestHandler {
 
     @Override
     public NanoHTTPD.Response onRequest(String root, Map<String, String> args, NanoHTTPD.IHTTPSession session) {
-        if (args.size() > 0)
-            ;
-        else {
-            String path;
-            if ("/".equals(session.getUri()))
-                path = root + "index.html";
-            else
-                path = root + session.getUri();
-            File file = new File(path);
-            if (file.exists()) {
-                try {
-                    FileInputStream is = new FileInputStream(file);
-                    // nano httpd will close the stream
-                    return newFixedLengthResponse(NanoHTTPD.Response.Status.OK,
-                            MimeHelper.getMime(file.getAbsolutePath()), is, file.length());
-                } catch (FileNotFoundException e) { // checked
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                // http code 404
-                return newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, MIME_PLAINTEXT, null);
+
+        String path;
+        if ("/".equals(session.getUri()))
+            path = root + "index.html";
+        else
+            path = root + session.getUri();
+        File file = new File(path);
+        if (file.exists()) {
+            try {
+                FileInputStream is = new FileInputStream(file);
+                // nano httpd will close the stream
+                return newFixedLengthResponse(NanoHTTPD.Response.Status.OK,
+                        MimeHelper.getMime(file.getAbsolutePath()), is, file.length());
+            } catch (FileNotFoundException e) { // checked
+                e.printStackTrace();
             }
+        } else {
+            // http code 404
+            return newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, MIME_PLAINTEXT, null);
         }
+
         return null;
     }
 }
