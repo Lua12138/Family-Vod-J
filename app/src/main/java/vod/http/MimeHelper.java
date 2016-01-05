bf9b3e7e027c6f8413d1623871855103b3fb17ca
forDream <gam2046@gmail.com>
2016-01-05 16:21:19 +0800

    [fix]Mime support

--- app/src/main/java/vod/http/MimeHelper.java
+++ app/src/main/java/vod/http/MimeHelper.java
@@ -1,27 +1,53 @@
 package vod.http;
 
-import eu.medsea.mimeutil.MimeType;
-import eu.medsea.mimeutil.MimeUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import vod.util.AppProperty;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.Iterator;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by forDream on 2015-12-26.
  */
 public class MimeHelper {
-    public static String getMime(String url) {
-        MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
-        File file = new File(url);
+    private static Logger logger = LoggerFactory.getLogger(MimeHelper.class);
 
-        if (file.exists() && !file.isDirectory()) {
-            Collection<MimeType> collection = MimeUtil.getMimeTypes(file);
-            for (Iterator<MimeType> iterator = collection.iterator(); iterator.hasNext(); ) {
-                MimeType type = iterator.next();
-                return type.toString();
+    public static Map<String, String> map;
+
+    static {
+        String dir = AppProperty.userDir() + File.separator;
+        ObjectMapper objectMapper = new ObjectMapper();
+        File config = new File(dir + "mime.json");
+        boolean bool = false;
+        if (config.exists()) {
+            try {
+                map = objectMapper.readValue(config, HashMap.class);
+                bool = true;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        return "unknown";
+
+        if (!bool) {
+            logger.error("load config error");
+            throw new Error("init mime helper fail.");
+        }
+    }
+
+    public static String getMime(String file) {
+        int dotPos = file.lastIndexOf(".");
+        String ext = file.substring(dotPos + 1).toLowerCase();
+        String mime = map.get(ext);
+        if (mime == null)
+            mime = "application/octet-stream";
+        return mime;
+    }
+
+    public static String getMime(File file) {
+        return getMime(file.getAbsolutePath());
     }
 }
